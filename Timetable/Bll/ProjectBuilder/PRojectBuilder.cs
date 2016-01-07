43e51b4e497e21f3a1eeaf4a6e3d398c97a956b7
Zhambul <e.zhambul@gmail.com>
2016-01-07 09:20:19 +0300

    Revert "init commit"
    This reverts commit 3338d0efc08102e4593f91ddc981cc7f61e7dc0c.

--- Timetable/Bll/ProjectBuilder/PRojectBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Timetable.Bll.Project;
-
-namespace Timetable.Bll.ProjectBuilder
-{
-    class PRojectBuilder : IProjectBuilder
-    {
-        public IProject Result { get; private set; }
-    }
-}
